f0b87b015079a1627b241319755e2e9992307243
Daniel Henrique de Carvalho Teixeira Alves <105288296+danielcta@users.noreply.github.com>
2024-04-09 11:34:24 -0300

    Add files via upload

---
+++ C08EX04.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class C08EX04
+{
+    public static void main(String[] args)
+    {
+        String name;
+        int age, more18, less18;
+
+        Scanner input = new Scanner(System.in);
+
+        for (int aux=1; aux <= 50; aux++)
+        {
+            System.out.print("");
+        }
+    }
+}
