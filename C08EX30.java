9b8060a504e5ab55743220c2aa7adb64951e9a78
Daniel Henrique de Carvalho Teixeira Alves <105288296+danielcta@users.noreply.github.com>
2024-04-30 09:32:34 -0300

    Update C08EX30.java

--- C08EX30.java
+++ C08EX30.java
@@ -1,49 +1,51 @@
 import javax.swing.*;
 
-public class C08EX30
-{
-    public static void main(String[] args)
-    {
+public class C08EX30 {
+    public static void main(String[] args) {
         double serie, x, n;
 
+        // Solicita o valor de x e verifica se é diferente de zero
         x = Double.parseDouble(JOptionPane.showInputDialog(null,
                 "Qual o valor de x?",
                 "Exercício 30",
                 JOptionPane.QUESTION_MESSAGE));
-        while (x == 0)
-        {
+        while (x == 0) {
             JOptionPane.showMessageDialog(null,
                     "x não deve ser equivalente a zero!",
                     "Exercício 30",
                     JOptionPane.ERROR_MESSAGE);
 
-            x = Long.parseLong(JOptionPane.showInputDialog(null,
+            x = Double.parseDouble(JOptionPane.showInputDialog(null,
                     "Qual o valor de x?",
                     "Exercício 30",
                     JOptionPane.QUESTION_MESSAGE));
         }
+
+        // Solicita o valor de n e verifica se é diferente de zero
         n = Double.parseDouble(JOptionPane.showInputDialog(null,
                 "Qual o valor de n?",
                 "Exercício 30",
                 JOptionPane.QUESTION_MESSAGE));
-        while (n == 0)
-        {
+        while (n == 0) {
             JOptionPane.showMessageDialog(null,
-                    "n não deve ser equivalente a zero!!",
+                    "n não deve ser equivalente a zero!",
                     "Exercício 30",
                     JOptionPane.ERROR_MESSAGE);
 
-            n = Long.parseLong(JOptionPane.showInputDialog(null,
+            n = Double.parseDouble(JOptionPane.showInputDialog(null,
                     "Qual o valor de n?",
                     "Exercício 30",
                     JOptionPane.QUESTION_MESSAGE));
         }
+
+        // Calcula a série usando o valor de x e n fornecidos
         serie = x;
 
-        for (int aux = 1; aux <= n; aux++)
-        {
-            serie += Math.pow(x, aux)/aux;
+        for (int aux = 1; aux <= n; aux++) {
+            serie += Math.pow(x, aux) / aux;
         }
+
+        // Imprime o resultado da série
         System.out.printf("S = %.2f", serie);
     }
 }
