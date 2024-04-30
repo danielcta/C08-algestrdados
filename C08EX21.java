ef00116211e9142428673c610174bce8db154665
Daniel Henrique de Carvalho Teixeira Alves <105288296+danielcta@users.noreply.github.com>
2024-04-30 09:22:50 -0300

    Update C08EX21.java

--- C08EX21.java
+++ C08EX21.java
@@ -1,34 +1,34 @@
 import javax.swing.*;
 
-public class C08EX21
-{
-    public static void main(String[] args)
-    {
+public class C08EX21 {
+    public static void main(String[] args) {
         String situacao;
         double altura, imc;
 
+        // Solicita a altura do usuário
         altura = Double.parseDouble(JOptionPane.showInputDialog(null,
                 "Qual a sua altura?",
                 "Exercício 21",
                 JOptionPane.QUESTION_MESSAGE));
 
+        // Exibe os cabeçalhos das colunas
         System.out.println("PESO..........IMC..........SITUAÇÃO CORPÓREA");
 
-        for (int peso = 60; peso <= 100; peso++)
-        {
+        // Loop para calcular e exibir o IMC para pesos de 60 kg a 100 kg
+        for (int peso = 60; peso <= 100; peso++) {
+            // Calcula o IMC para o peso atual
             imc = peso / Math.pow(altura, 2);
-            if (imc < 20)
-            {
-                situacao = "ABAIXO DO PESO";
 
-            } else if (imc >= 20 && imc <= 25)
-            {
+            // Determina a situação corporal com base no IMC calculado
+            if (imc < 20) {
+                situacao = "ABAIXO DO PESO";
+            } else if (imc >= 20 && imc <= 25) {
                 situacao = "PESO IDEAL";
-            }
-            else
-            {
+            } else {
                 situacao = "ACIMA DO PESO";
             }
+
+            // Exibe os dados formatados
             System.out.printf("%dkg..........%.2f..........%s\n", peso, imc, situacao);
         }
     }
