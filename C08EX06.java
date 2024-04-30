7693c383d03706bcf05b239bf06f77f9e44fc9cf
Daniel Henrique de Carvalho Teixeira Alves <105288296+danielcta@users.noreply.github.com>
2024-04-30 09:01:37 -0300

    Update C08EX06.java

--- C08EX06.java
+++ C08EX06.java
@@ -1,41 +1,40 @@
 import javax.swing.*;
 
-public class C08EX06
-{
-    public static void main(String[] args)
-    {
-
+public class C08EX06 {
+    public static void main(String[] args) {
+        // Variáveis para armazenar o número digitado, a soma dos números divisíveis por 4 e a contagem de números pares, ímpares e divisíveis por 3
         long indice, soma = 0;
         int par = 0, impar = 0, diviseis3 = 0;
 
+        // Loop para solicitar ao usuário que insira um número por 10 vezes
         for (int aux = 1; aux <= 10; aux++) {
-            indice = Integer.parseInt(JOptionPane.showInputDialog(null,
-                    "Informe  número:",
+            // Solicita ao usuário que insira um número e armazena o valor digitado na variável indice
+            indice = Long.parseLong(JOptionPane.showInputDialog(null,
+                    "Informe o número:",
                     "Exercício 6",
                     JOptionPane.QUESTION_MESSAGE));
 
-
-            if (indice % 2 == 0)
-            {
+            // Verifica se o número é par ou ímpar e incrementa a contagem correspondente
+            if (indice % 2 == 0) {
                 par++;
                 System.out.printf("O número %d é par\n", indice);
-
-            }  else
-            {
+            } else {
                 impar++;
                 System.out.printf("O número %d é ímpar\n", indice);
-
             }
-            if (indice % 3 == 0)
-            {
+            
+            // Verifica se o número é divisível por 3 e incrementa a contagem de números divisíveis por 3
+            if (indice % 3 == 0) {
                 diviseis3++;
-
             }
-            if (indice % 4 == 0)
-            {
+            
+            // Verifica se o número é divisível por 4 e adiciona o número à soma se for o caso
+            if (indice % 4 == 0) {
                 soma += indice;
             }
         }
+        
+        // Imprime a soma dos números divisíveis por 4 e o número de números divisíveis por 3
         System.out.printf("A soma dos números informados divisíveis por 4 é igual a %d\nForam digitados %d números que são divisíveis por 3", soma, diviseis3);
     }
 }
